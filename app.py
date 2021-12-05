06aa61b1a2525a570aeb97318c04a7ecc649c084
Hanan Hanafi <hananhanafi76@gmail.com>
2021-12-05 23:11:59 +0700

    Add Machine Learning Model & create API request for prediction with POST method

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+import json
+from flask import Flask, request, jsonify
+import pickle
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+loaded_model = pickle.load(open('finalized_model.sav', 'rb'))  # load model
+
+
+@app.route('/', methods=['POST'])
+def get_prediction():
+    req_json = json.loads(request.data)  # read request
+    age = req_json['age']
+    weight = req_json['weight']
+    height = req_json['height']
+
+    bmi = req_json['weight']/((req_json['height']/100)**2)  # BMI Calculation
+    x = [[weight, age, height, bmi]]  # Save array of request data into variable x
+
+    result = loaded_model.predict(x)  # predict
+    result_str = [str(i) for i in result]  # save the result
+    return jsonify({'output': result_str})  # return model output
+
 
 if __name__ == '__main__':
     app.run()
