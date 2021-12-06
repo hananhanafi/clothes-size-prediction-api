3236e8dfca7949a8c86db0513a4cd31a9aeeda5b
Hanan Hanafi <hananhanafi76@gmail.com>
2021-12-06 22:07:42 +0700

    Heroku hosting

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import pickle
 
 app = Flask(__name__)
 
+loaded_model = pickle.load(open('finalized_model.sav', 'rb'))  # load model
+
 
 @app.route('/predict/', methods=['POST'])
 def get_prediction():
-    loaded_model = pickle.load(open('finalized_model.sav', 'rb'))  # load model
     req_json = json.loads(request.data)  # read request
     age = req_json['age']
     weight = req_json['weight']
