302f336ffc551ec6caedac5eee1a9cbc0e684a24
Hanan Hanafi <hananhanafi76@gmail.com>
2021-12-07 22:50:51 +0700

    Add Flask Cors

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
-
 app = Flask(__name__)
-
+CORS(app)
 loaded_model = pickle.load(open('finalized_model.sav', 'rb'))  # load model
 
 
@@ -13,10 +13,8 @@ def get_prediction():
     age = req_json['age']
     weight = req_json['weight']
     height = req_json['height']
-
     bmi = req_json['weight']/((req_json['height']/100)**2)  # BMI Calculation
     x = [[weight, age, height, bmi]]  # Save array of request data into variable x
-
     result = loaded_model.predict(x)  # predict
     result_str = [str(i) for i in result]  # save the result
     return jsonify({'output': result_str})  # return model output
@@ -30,3 +28,4 @@ def index():
 
 if __name__ == '__main__':
     app.run()
+
