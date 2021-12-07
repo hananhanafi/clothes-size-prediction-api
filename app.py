d2623b5787210187d5276b07221211b21b200fdf
Hanan Hanafi <hananhanafi76@gmail.com>
2021-12-08 00:10:11 +0700

    Add web UI

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 import json
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify, render_template, send_from_directory
 import pickle
 app = Flask(__name__)
-CORS(app)
 loaded_model = pickle.load(open('finalized_model.sav', 'rb'))  # load model
 
 
 @app.route('/predict/', methods=['POST'])
 def get_prediction():
     req_json = json.loads(request.data)  # read request
-    age = req_json['age']
-    weight = req_json['weight']
-    height = req_json['height']
-    bmi = req_json['weight']/((req_json['height']/100)**2)  # BMI Calculation
+    age = int(req_json['age'])
+    weight = int(req_json['weight'])
+    height = int(req_json['height'])
+    bmi = weight/((height/100)**2)  # BMI Calculation
     x = [[weight, age, height, bmi]]  # Save array of request data into variable x
     result = loaded_model.predict(x)  # predict
     result_str = [str(i) for i in result]  # save the result
@@ -23,7 +21,8 @@ def get_prediction():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    # return "<h1>Welcome to our server !!</h1>"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
